a89cec13114080f7eaac4a4a776af7b754e0a49e
Leonardo <leonardo.a.lessa@gmail.com>
2014-05-17 17:27:25 -0300

    Translate to tutorial 4 before depth corretion.

--- Main.hs
+++ Main.hs
@@ -94,7 +94,8 @@ localizateAttrib attrib programID =
 
 -- List of all variables attributes
 attribList :: [String]
-attribList = [ "vertexPosition_modelspace" ]
+attribList = [ "vertexPosition_modelspace"
+             , "vertexColor"]
 
 localizateAllAttrib :: GLuint -> IO [GLuint]
 localizateAllAttrib programID = do
@@ -121,7 +122,7 @@ localizateVarying varying programID =
 
 -- List of all unifrom attributes
 varyingList :: [String]
-varyingList = [ "color" ]
+varyingList = [ ]
 
 localizateAllVarying :: GLuint -> IO [GLint]
 localizateAllVarying programID = do
@@ -152,6 +153,28 @@ lookAt up eye target = orientationM `multmm` translationM
                   (0:.0 :.0:.1:.()) :. ()
     translationM = translation (-eye)
 
+-- Fill a new buffer with data
+fillNewBuffer :: [GLfloat] -> IO GLuint
+fillNewBuffer list = do
+  bufId <- withNewPtr (glGenBuffers 1)
+  glBindBuffer gl_ARRAY_BUFFER bufId
+  withArrayLen list $ \length ptr ->
+    glBufferData gl_ARRAY_BUFFER (fromIntegral (length *
+                                  sizeOf (undefined :: GLfloat)))
+                 (ptr :: Ptr GLfloat) gl_STATIC_DRAW
+  return bufId
+
+bindBufferToAttrib :: GLuint -> GLuint -> IO ()
+bindBufferToAttrib bufId attribLoc = do
+  glEnableVertexAttribArray attribLoc
+  glBindBuffer gl_ARRAY_BUFFER bufId
+  glVertexAttribPointer attribLoc  -- attribute location in the shader
+                        3  -- 3 components per vertex
+                        gl_FLOAT  -- coordinates type
+                        (fromBool False)  -- normalize?
+                        0  -- stride
+                        nullPtr  -- vertex buffer offset
+
 main :: IO ()
 main = do
     window <- initAll
@@ -164,22 +187,83 @@ main = do
     programID <- loadProgram "SimpleVertexShader.vertexshader"
                              "SimpleFragmentShader.fragmentshader"
 
-    [vertexPosition_modelspaceID] <- localizateAllAttrib programID
+    [vertexPosition_modelspaceID, vertexColor] <- localizateAllAttrib programID
     [mvpID] <- localizateAllUniform programID
-    [color] <- localizateAllVarying programID
 
-    let g_vertex_buffer_data = [-1, -1, 0, -- Triangle's vertex
-                                 1, -1, 0,
-                                 0,  1, 0] :: [GLfloat]
-
-    vertexBufferName <-
-      alloca $ \vertexBufferPtr -> do
-        glGenBuffers 1 vertexBufferPtr
-        peek vertexBufferPtr
-    glBindBuffer gl_ARRAY_BUFFER vertexBufferName
-    withArrayLen g_vertex_buffer_data $ \len ptr -> do
-      let size = fromIntegral $ len * sizeOf (head g_vertex_buffer_data)
-      glBufferData gl_ARRAY_BUFFER size (ptr :: Ptr GLfloat) gl_STATIC_DRAW
+    let vertexBufferData = [-1.0,-1.0,-1.0, -- triangle 1 : begin
+                            -1.0,-1.0, 1.0,
+                            -1.0, 1.0, 1.0, -- triangle 1 : end
+                            1.0, 1.0,-1.0, -- triangle 2 : begin
+                            -1.0,-1.0,-1.0,
+                            -1.0, 1.0,-1.0, -- triangle 2 : end
+                            1.0,-1.0, 1.0,
+                            -1.0,-1.0,-1.0,
+                            1.0,-1.0,-1.0,
+                            1.0, 1.0,-1.0,
+                            1.0,-1.0,-1.0,
+                            -1.0,-1.0,-1.0,
+                            -1.0,-1.0,-1.0,
+                            -1.0, 1.0, 1.0,
+                            -1.0, 1.0,-1.0,
+                            1.0,-1.0, 1.0,
+                            -1.0,-1.0, 1.0,
+                            -1.0,-1.0,-1.0,
+                            -1.0, 1.0, 1.0,
+                            -1.0,-1.0, 1.0,
+                            1.0,-1.0, 1.0,
+                            1.0, 1.0, 1.0,
+                            1.0,-1.0,-1.0,
+                            1.0, 1.0,-1.0,
+                            1.0,-1.0,-1.0,
+                            1.0, 1.0, 1.0,
+                            1.0,-1.0, 1.0,
+                            1.0, 1.0, 1.0,
+                            1.0, 1.0,-1.0,
+                            -1.0, 1.0,-1.0,
+                            1.0, 1.0, 1.0,
+                            -1.0, 1.0,-1.0,
+                            -1.0, 1.0, 1.0,
+                            1.0, 1.0, 1.0,
+                            -1.0, 1.0, 1.0,
+                            1.0,-1.0, 1.0]
+        colorBufferData  = [0.583,  0.771,  0.014,
+                            0.609,  0.115,  0.436,
+                            0.327,  0.483,  0.844,
+                            0.822,  0.569,  0.201,
+                            0.435,  0.602,  0.223,
+                            0.310,  0.747,  0.185,
+                            0.597,  0.770,  0.761,
+                            0.559,  0.436,  0.730,
+                            0.359,  0.583,  0.152,
+                            0.483,  0.596,  0.789,
+                            0.559,  0.861,  0.639,
+                            0.195,  0.548,  0.859,
+                            0.014,  0.184,  0.576,
+                            0.771,  0.328,  0.970,
+                            0.406,  0.615,  0.116,
+                            0.676,  0.977,  0.133,
+                            0.971,  0.572,  0.833,
+                            0.140,  0.616,  0.489,
+                            0.997,  0.513,  0.064,
+                            0.945,  0.719,  0.592,
+                            0.543,  0.021,  0.978,
+                            0.279,  0.317,  0.505,
+                            0.167,  0.620,  0.077,
+                            0.347,  0.857,  0.137,
+                            0.055,  0.953,  0.042,
+                            0.714,  0.505,  0.345,
+                            0.783,  0.290,  0.734,
+                            0.722,  0.645,  0.174,
+                            0.302,  0.455,  0.848,
+                            0.225,  0.587,  0.040,
+                            0.517,  0.713,  0.338,
+                            0.053,  0.959,  0.120,
+                            0.393,  0.621,  0.362,
+                            0.673,  0.211,  0.457,
+                            0.820,  0.883,  0.371,
+                            0.982,  0.099,  0.879]
+    vertexBufferName <- fillNewBuffer vertexBufferData
+    colorBufferId <- fillNewBuffer colorBufferData
 
     --Drawing
     GLFW.swapInterval 1
@@ -189,12 +273,12 @@ main = do
         glClear gl_DEPTH_BUFFER_BIT
         glUseProgram programID
         with (mvpMatrix ) $
-          glUniformMatrix4fv mvpID 1 (fromBool False) . castPtr
-        glEnableVertexAttribArray vertexPosition_modelspaceID
-        glBindBuffer gl_ARRAY_BUFFER vertexBufferName
-        glVertexAttribPointer vertexPosition_modelspaceID 3 gl_FLOAT (fromBool False) 0 nullPtr
-        glDrawArrays gl_TRIANGLES 0 3 -- The drawing!
+          glUniformMatrix4fv mvpID 1 (fromBool True) . castPtr
+        bindBufferToAttrib vertexBufferName vertexPosition_modelspaceID
+        bindBufferToAttrib colorBufferId vertexColor
+        glDrawArrays gl_TRIANGLES 0 (12 *3) -- The drawing!
         glDisableVertexAttribArray vertexPosition_modelspaceID
+        glDisableVertexAttribArray vertexColor
         GLFW.swapBuffers window
         GLFW.pollEvents
         t1 <- fromJust <$> GLFW.getTime -- Final time
